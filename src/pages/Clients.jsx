f1cbf9845f7742e654df9f2cb20c773218764bcf
Mohib Khan <khanmohib2001@gmail.com>
2026-01-28 22:45:56 +0500

    Add client management components and update styles

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../components/common/Header'
+import ClientCardsGrid from '../components/clients/ClientCardsGrid'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className='h-screen w-full p-8 flex flex-col gap-10'>
+      <Header HeaderName={"Clients"} HeaderDescription={"Manage your client contacts and information"}/>
+      <ClientCardsGrid/>
+    </div>
   )
 }
 
